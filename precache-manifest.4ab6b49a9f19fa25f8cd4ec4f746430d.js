a6b628cd727fced6fa41ff2be207ed7a8c9e6b31
xanaks <36263837+xanaks@users.noreply.github.com>
2020-08-15 01:39:50 +0400

    Add files via upload

---
+++ precache-manifest.4ab6b49a9f19fa25f8cd4ec4f746430d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b4d79da026fb013aebd4cd6d1952d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0706123c919620d7b9ba",
+    "url": "/static/css/main.e1433f76.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0706123c919620d7b9ba",
+    "url": "/static/js/main.e19f40a8.chunk.js"
+  },
+  {
+    "revision": "c0ccae055cb2c07e921a",
+    "url": "/static/js/runtime-main.4483ebb2.js"
+  }
+]);
