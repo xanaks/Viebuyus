0aa6f98c85baf0314c22e0dbf964bf5a9a1c0314
xanaks <36263837+xanaks@users.noreply.github.com>
2020-08-15 01:44:00 +0400

    Add files via upload

---
+++ precache-manifest.cd858d266364daf0cb42b93405d3f629.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e8d995acf2770c4ffde5b64a02b86d",
+    "url": "/Viebuyus/index.html"
+  },
+  {
+    "revision": "0706123c919620d7b9ba",
+    "url": "/Viebuyus/static/css/main.e1433f76.chunk.css"
+  },
+  {
+    "revision": "ab12b72ec6cd5010e57a",
+    "url": "/Viebuyus/static/js/2.42df7b18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Viebuyus/static/js/2.42df7b18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0706123c919620d7b9ba",
+    "url": "/Viebuyus/static/js/main.e19f40a8.chunk.js"
+  },
+  {
+    "revision": "64555090b075d114c674",
+    "url": "/Viebuyus/static/js/runtime-main.83a03c5a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ab6b49a9f19fa25f8cd4ec4f746430d.js"
+  "/Viebuyus/precache-manifest.cd858d266364daf0cb42b93405d3f629.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Viebuyus/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
